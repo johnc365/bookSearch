9743c5803d6f20b339105c994f493f7df32c32d2
johnc365 <johnrcaldwell123@gmail.com>
2024-11-23 13:43:16 -0600

    modified server side code

---
+++ server/src/schemas/resolvers.ts
@@ -0,0 +1,53 @@
+import User, { UserDocument } from '../models/User.js';
+import { signToken, AuthenticationError } from '../services/auth.js';
+
+const resolvers = {
+  Query: {
+    me: async (): Promise<UserDocument[] | null> => {
+      return User.find({});
+    },
+    matchups: async (_parent: any, { _id }: { _id: string }): Promise<UserDocument[] | null> => {
+      const params = _id ? { _id } : {};
+      return User.find(params);
+    },
+  },
+  Mutation: {
+    createUser: async (_parent: any, args: any) => {
+      const user = await User.create(args);
+      const token = signToken(user.username, user.password, user._id);
+      return {user, token};
+    },
+    login: async (_parent: any, { email, password }: { email: string, password: string}) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        return AuthenticationError;
+      }
+      const token = signToken(user.username, user.password, user._id);
+      return ({ token, user });
+    },
+    saveBook: async (_parent: any, args: any, context: any) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $addToSet: { savedBooks: {...args} } },
+        { new: true, runValidators: true }
+      );
+      return updatedUser;
+    },
+    deleteBook: async (_parent: any, args: any, context: any) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: {bookId: args.bookId} } },
+        { new: true, runValidators: true }
+      );
+      return updatedUser;
+    },
+  },
+};
+
+export default resolvers;
