0861cc6bae55b429b6f49f4e75f567121faca9c6
johnc365 <johnrcaldwell123@gmail.com>
2024-11-26 09:19:18 -0600

    debugs

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,11 @@ import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/serve
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './services/auth.js';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename= fileURLToPath(import.meta.url);
+const __dirname= dirname(__filename);
 
 const server = new ApolloServer({
   typeDefs,
