3fcadebea34a284151f9886917ff7710a7f6c536
johnc365 <johnrcaldwell123@gmail.com>
2024-11-26 09:25:58 -0600

    depoloyment debug

--- server/src/server.ts
+++ server/src/server.ts
@@ -34,10 +34,10 @@ const startApolloServer = async () => {
   ));
 console.log(process.env);
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
 
